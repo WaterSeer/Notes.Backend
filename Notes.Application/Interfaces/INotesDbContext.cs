c09a82c85c04247526090bb441df1c967fe52856
Mike Kargin <gray_cloud@mail.ru>
2022-09-11 00:22:18 +0300

    Добавьте файлы проекта.

---
+++ Notes.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; } 
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Notes.Persistence/NotesDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using Notes.Persistence.EntityTypeConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Persistence
+{
+    public class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set ; }
+
+        public NotesDbContext(DbContextOptions<NotesDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(builder);
+        }
+
+
+    }
+}
